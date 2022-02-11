ce68c0dbd1d227eb9fc30597dac7629789cb56ac
brigand <dylan.callahan@raytheon.com>
2022-02-10 23:06:05 -0500

    Shaping nono work obj

--- nono_word.py
+++ nono_word.py
@@ -1,13 +1,20 @@
 import discord
 
 class NoNo_Word():
-    def __init__(self, word: str):
+    def __init__(self, word: str, count: int = 0, jump_url: str = ''):
         self.word = word # The nono_word iself
-        self.count = 0
-        self.jump_urls = [] # an array of url strings to all ocurrances of the nono_word
+        self.count = count
+        self.jump_urls = [jump_url] # an array of url strings to all ocurrances of the nono_word
     
-    def update_count(self, message_string):
-        self.count += message_string.count(self.word)
+    # Update word count with a message broken into a list of strings
+    def update_count(self, message_list: list):
+        self.count += message_list.count(self.word)
 
+    # Add reference url to list
     def add_jump_url(self, url: str):
         self.jump_urls.append(url)
+
+    # Update all fields
+    def update(self, message_list: list, url :str):
+        self.update_count(message_list)
+        self.add_jump_url(url)
